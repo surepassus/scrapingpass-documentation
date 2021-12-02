b5b5890cbf8ed9606ceb3c2a973b23729c422be4
weibeu <deepakrajko14@gmail.com>
2021-12-02 23:17:57 +0530

    Layout initial project

---
+++ docusaurus.config.js
@@ -0,0 +1,74 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Scrapingpass API',
+  tagline: 'Web Scraping API and Robotic process automation (RPA)',
+  url: 'http://localhost:3000',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/scrapingpass-icon.png',
+  organizationName: 'surepass', // Usually your GitHub org/user name.
+  projectName: 'scrapingpass-documentation', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/'
+          // Please change this to your repo.
+          // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Scrapingpass API',
+        logo: {
+          alt: 'Scrapingpass Logo',
+          src: 'img/scrapingpass-icon.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'api-overview',
+            position: 'left',
+            label: 'Documentation',
+          },
+          { 
+            href: 'https://scrapingpass.com/blog/', 
+            label: 'Blog', 
+            position: 'left' 
+          },
+          {
+            href: 'https://scrapingpass.com/pricing/',
+            label: 'Try for Free',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} <a href="https://surepass.io/" target="_blank">Surepass Technologies</a>.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
